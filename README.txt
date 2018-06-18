29b3e283b32d377567f9cb2cec8e0132b851b5a1
clm0ss <clm0ss@users.noreply.github.com>
2018-06-18 16:41:04 -0500

    Initial commit

---
+++ README.txt
@@ -0,0 +1,20 @@
+
+DOCUMENTATION:
+
+Enter 'exit' at "username","UCS IP", or "password" prompt to exit program, or just use the red X button.
+
+There is NO error checking at the prompts so type carefully.
+
+File path should be formatted, for example as, '/folder/huu.iso'.
+
+Supports only the following file sharing protocols: nfs,cifs,www. I recommend using free software FreeNAS, and/or HFS (HTTP File Server). Dedicated NAS is ideal.
+
+If connection to server fails before update attempt sent, its entry will be deleted as to not disrupt the update process for other servers in list.
+
+The update monitoring function does not run in order nor does it identify the server being monitored, this can be developed further in the future.
+
+WARNING: May encounter problems with older CIMC versions i.e 1.47/1.5
+
+Coded by clmoss@cisco.com (Clayton M. Moss)
+
+Please contact clmoss@cisco.com if any bugs are encountered, or for feature requests. I plan to add features in the future.
---
+++ UCS-IMC-FirmwareUpdater-1.2-FINAL(clmoss@cisco.com).py
@@ -0,0 +1,138 @@
+import multiprocessing
+from multiprocessing import Pool
+from multiprocessing import Process
+from imcsdk.utils.imcfirmwareinstall import firmware_huu_update
+from imcsdk.utils.imcfirmwareinstall import firmware_huu_update_monitor
+from imcsdk.imchandle import ImcHandle
+import time
+import datetime
+import imcsdk.imchandle
+import getpass
+import time
+import sys
+import os
+
+ucs_hosts = []
+user_database = []
+passwds = []
+handles = {}
+
+def connect():
+    count = 0
+    increment = 0
+#stores multiple handle calls in a dictionary
+    for hosts in ucs_hosts:
+        key = count
+        handle = ImcHandle(hosts,user_database[count],passwds[count],timeout=None)
+        handles[key] = handle
+        count += 1
+#attempts to login to each handle/server, odd retry thing I came up with.        
+    for key in handles:
+        increment += 1
+        try:
+            handles[key].__enter__()
+        except:
+            handles[key].__enter__()
+        else:
+            pass
+#confirms connectivity to server, or else removes handle 
+        if handles[key]._validate_connection() == False:
+            try:
+                handles[key].__enter__()
+            except:
+                print("Connection to server #%d Failed.." % (increment))
+                print("")
+                print("Deleting entry for server #%d and continuing.." % (increment))
+                del handles[increment - 1]
+                del ucs_hosts[increment - 1]
+                del user_database[increment - 1]
+                del passwds[increment - 1]
+                pass
+            else:
+                pass
+       
+        if handles[key]._validate_connection() == True:
+            print("Connection to UCS Server #%d in list, successful.." % (increment))
+        
+    file_server = input("Enter file server IP or localhost: ")
+    server_login = input("Does file server need login? [y/n]: ").lower()
+    if server_login == 'y':
+        user = input("Enter Username: ")
+        password = getpass.getpass()
+    elif server_login == 'n':
+            user = ""
+            password = ""
+    else:
+        print('Invalid Input, using defaults "n"..')
+        user = ""
+        password = ""
+  
+    protocol = input("Enter file sharing protocol ('www','cifs','nfs'): ").lower()
+    directory = input("Enter fully specified path to file: ")
+    up_all = "all"
+    stop = "yes"
+    time = 60
+    timeout = 240
+    interval = 30
+    no = "no"
+    tick = 0
+
+    for key in handles:
+        if handles[key]._validate_connection() == True:
+            tick += 1
+            session = handles[key]
+            print("")
+            print("Sending update request to server #%d.." % (tick))
+
+            p = Process(target=firmware_huu_update, args=(session,directory,protocol,file_server,user,password,up_all,stop,timeout,no,no))
+            p.start()
+            p.join()
+
+            monitor = multiprocessing.Pool(None)
+            monitor.apply_async(firmware_huu_update_monitor, args=(session,time,interval))
+                     
+        else:
+            print("Connection failed to server #%d in list, continuing.." % (tick))
+    monitor.close()
+    monitor.join()    
+
+    for key in handles:
+        handles[key]._logout()
+        print("Server #%d Updated.." % (int(key) + 1))
+
+def main():
+    end_program = 'exit'
+    
+    def end(a):
+        if a == end_program:
+            return sys.exit()
+        else:
+            return
+
+    while True:
+        host_ips = input("Enter UCS CIMC IP:  ").lower()
+        end(host_ips)
+        username = input("Enter Username: ")
+        end(username)
+        password = getpass.getpass()
+        ucs_hosts.append(host_ips)
+        user_database.append(username)
+        passwds.append(password)
+        print ("Servers Added to update list:  " + str(len(ucs_hosts)))
+        add_hosts = input('Add UCS Servers to update list? [y/n]: ').lower()
+
+        if add_hosts == 'n':
+            print("Initiating connection(s) to UCS Server(s) in list..")
+            return connect()
+        elif add_hosts == 'y':
+            continue
+        else:
+            print("Invalid Input..")
+            continue
+
+if __name__ == '__main__':
+    print("")
+    print("Cisco UCS Server Firmware Update Automation Tool - Made by clmoss@cisco.com")
+    print("")
+    multiprocessing.freeze_support()
+    main()
